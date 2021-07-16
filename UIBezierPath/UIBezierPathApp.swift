f6184b5b34a969ed9b69911b90bcf89484dd6ac9
JY <rd-sw13@visionsecurity.com.tw>
2021-07-16 10:54:00 +0800

    Initial Commit

---
+++ UIBezierPath/UIBezierPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBezierPathApp.swift
+//  UIBezierPath
+//
+//  Created by JianYu on 2021/7/16.
+//
+
+import SwiftUI
+
+@main
+struct UIBezierPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
